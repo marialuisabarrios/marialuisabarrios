8ebfe376586a006bddc2ca90add17305b685099b
marialuisabarrios <98150919+marialuisabarrios@users.noreply.github.com>
2022-01-28 09:52:39 +0100

    richiesta del nome

---
+++ maria.barrios/maria.barrios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maria.barrios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
